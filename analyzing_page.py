fe70beec871dfbbec33cc819aa4d2aba40c3c311
hannes1112 <49373937+hannes1112@users.noreply.github.com>
2020-06-28 22:53:01 +0200

    Update analyzing_page.py

--- analyzing_page.py
+++ analyzing_page.py
@@ -1,16 +1,17 @@
 import urllib.request
 from io import StringIO
 from html.parser import HTMLParser
+from collections import Counter
+import operator
 
 #----------------------------------------
 fp = urllib.request.urlopen("http://www.python.org")#URL
 mybytes = fp.read()
-global html_var
 html_var = mybytes.decode("utf8")
 fp.close()
 #database access
-#get data
 #----------------------------------------
+
 class MLStripper(HTMLParser):
     def __init__(self):
         super().__init__()
@@ -34,22 +35,39 @@ def linkfinder(html_var):
             links_numb += 1
         j += 1
     print("Links:"+str(links_numb))
+    return links_numb
 
-def countwordnothtml(html):#count wihtout html
+def full_to_raw(html):
+    global rowhtml
     s = MLStripper()
     s.feed(html)
     rowhtml = s.get_data()
+    return rowhtml
+
+def countwordnothtml(html):#count wihtout html
+    rowhtml = full_to_raw(html)
     words_numb = len(rowhtml.split()) 
     print("words no html:"+str(words_numb))
+    return words_numb
 
 def countwords(html_var):#raw
     words_numb = len(html_var.split()) 
     print("word with html:"+str(words_numb))
+    return words_numb
 
+def countwordsmultiple():#rowhtml
+    x=dict(Counter(rowhtml.split()))
+    print (type(x))
+    #sort
+    sorted_x = sorted(x.items(), key=operator.itemgetter(1))
+    print(sorted_x)
+    #blacklist ex. "and"
+    
 def main():
-    countwords(html_var)
-    linkfinder(html_var)
-    countwordnothtml(html_var)
+    cw=countwords(html_var)
+    lf=linkfinder(html_var)
+    cwh=countwordnothtml(html_var)
+    countwordsmultiple()
     
 if __name__ == "__main__":
     main()
